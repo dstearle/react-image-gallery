86628f0fcfe11b583a0ee6bc83951908999c4200
dstearle <dallas.st.earle@gmail.com>
2020-04-22 01:04:27 -0700

    Created fetch request for getting pixabay api key and returning images

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
 
+  useEffect(() => {
+
+    // Fetch for api key to get images from pixabay
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    .then(res => res.json())
+	.then(data => 
+		{
+			setImages(data.hits);
+			setIsLoading(false);
+		}
+	)
+    .catch(err => console.log(err));
+
+  }, []);
+
   return (
 
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
