b7fe98181905bc7a473e0b7131b086982a08d914
dstearle <dallas.st.earle@gmail.com>
2020-04-21 22:06:20 -0700

    Created basic card example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 
 function App() {
+
   return (
-    <div>
-      <h1 className="test-3xl">Hello World</h1>
+
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+
+      {/* Image */}
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+
+      {/* Content */}
+      <div className="px-6 py-4">
+
+        {/* Author */}
+        <div className="font-bold text-purple-500 text-xl mb-2"> Photo By: Jim Dean</div>
+
+        <ul>
+
+          <li>
+            <strong>Views: </strong> 4000
+          </li>
+
+          <li>
+            <strong>Downloads: </strong> 300
+          </li>
+
+          <li>
+            <strong>Likes: </strong> 20
+          </li>
+
+        </ul>
+
+      </div>
+      
     </div>
+
   );
+
 }
 
 export default App;
