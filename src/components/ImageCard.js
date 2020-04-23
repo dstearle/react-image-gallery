424bac015baab13508bf91a5287c1ab9570de86b
dstearle <dallas.st.earle@gmail.com>
2020-04-23 15:30:32 -0700

    Created loading animation for card grid and fixed unique key for card tags

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -43,7 +43,7 @@ const ImageCard = ({ image }) => {
                 {
                     tags.map(
                         
-                        tag => (<span key={image.id} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#{tag}</span>)
+                        (tag, index) => (<span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#{tag}</span>)
 
                     )
                     
